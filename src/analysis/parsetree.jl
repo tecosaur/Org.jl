978977e4f030efe72430f48f12708f6632143b7c
TEC <tec@tecosaur.com>
2021-11-30 10:33:36 +0800

    Ooops, some missing files

---
+++ src/analysis/parsetree.jl
@@ -0,0 +1,65 @@
+parsetree(org::Org, maxdepth=-1) = parsetree(stdout, org, maxdepth)
+parsetree(org::OrgComponent, maxdepth=-1) = parsetree(stdout, org, maxdepth)
+
+function parsetree(io::IO, org::Org, maxdepth=-1, depth=0)
+    printstyled(io, "Org Parse Tree", color=:yellow)
+    if length(org.contents) == 0
+        printstyled(io, " (empty)\n", color=:light_black)
+    else
+        print(io, '\n')
+        parsetree.(Ref(io), org.contents, maxdepth, depth+1)
+    end
+    nothing
+end
+
+function printstructure(io::IO, component::AbstractString, description,
+                        contents, maxdepth, depth, color=:yellow, bold=false)
+    printstyled(io, ' '^4depth, component; bold, color)
+    if !isnothing(description)
+        printstyled(io, ' ', description, color=:green)
+    end
+    if isnothing(contents)
+        print(io, '\n')
+    elseif contents isa Vector && length(contents) == 0
+        printstyled(io, " (empty)\n", color=:light_black)
+    elseif ! (contents isa Vector{<:OrgComponent})
+        print(io, '\n')
+    elseif depth == maxdepth
+        printstyled(io, " [hidden]\n", color=:light_black)
+    else
+        print(io, '\n')
+        parsetree.(Ref(io), contents, maxdepth, depth+1)
+    end
+    nothing
+end
+
+parsetree(io::IO, h::Heading, maxdepth::Integer, depth::Integer) =
+    printstructure(io, "Heading", '(' * h.title * ')', if isnothing(h.section); [] else [h.section] end, maxdepth, depth, :yellow, true)
+
+parsetree(io::IO, s::Section, maxdepth::Integer, depth::Integer) =
+    printstructure(io, "Section", nothing, s.contents, maxdepth, depth, :yellow, true)
+
+parsetree(io::IO, l::L, maxdepth::Integer, depth::Integer) where {L <: List} =
+    printstructure(io, string(L), l.items[1].bullet, l.items, maxdepth, depth, :magenta, true)
+
+function parsetree(io::IO, component::T, maxdepth::Integer, depth::Integer=0) where {T <: OrgComponent}
+    contents = if :contents in fieldnames(T) &&
+        component.contents isa Vector{<:OrgComponent}
+        component.contents
+    end
+    bold, color = if T <: OrgGreaterElement
+        (false, :magenta)
+    elseif T <: OrgElement
+        (false, :blue)
+    else
+        (false, :default)
+    end
+    printstructure(io, structurename(component), structuredesc(component),
+                   contents, maxdepth, depth, color, bold)
+end
+
+structurename(::C) where {C<:OrgComponent} = string(nameof(C))
+structuredesc(_::OrgComponent) = nothing
+
+structurename(t::TextMarkup) = "Text" * uppercasefirst(string(t.type))
+structuredesc(t::TextMarkup) = string(t.marker)
---
+++ src/render/formatting.jl
@@ -0,0 +1,72 @@
+function layouttable(io::IO, table::Table, tchars::Dict, indent=0)
+    colwidths = map(r -> if r isa TableRow
+                        length.(r.cells)
+                    end, table.rows) |>
+                        rs -> filter(!isnothing, rs) |>
+                        rs -> maximum(hcat(rs...); dims=2)
+    for row in table.rows
+        print(io, ' '^indent)
+        if row isa TableHrule
+            print(io, tchars['>'], join(repeat.(tchars['-'], colwidths .+ 2), tchars['+']), tchars['<'], "\n")
+        else
+            print(io, tchars['|'])
+            for (cell, fillwidth) in zip(row.cells, colwidths)
+                print(tchars['['], rpad(cell.contents, fillwidth), tchars[']'], tchars['|'])
+            end
+            row === last(table.rows) || print(io, '\n')
+        end
+    end
+end
+
+"""
+    wraplines(s::AbstractString, width::Integer, offset::Integer)
+`s` is assumed to only contain significant whitespace, and no newlines
+"""
+function wraplines(s::AbstractString, width::Integer, offset::Integer=0)
+    lines = SubString{String}[]
+    i, lastwrap, slen = 1, 0, ncodeunits(s)
+    mostrecentbreakoppotunity = 1
+    while i < slen
+        if s[i] == ' '
+            mostrecentbreakoppotunity = i
+        elseif s[i] == '\e'
+            if s[i+1] == '[' # Control Sequence Introducer
+                terminator = findfirst('m', @inbounds @view s[i:end])
+                delta = if !isnothing(terminator) terminator else 0 end
+                offset -= delta
+                i += delta
+            elseif s[i+1] == ']' # Operating System Command
+                terminator = findfirst("\e\\", @inbounds @view s[i:end])
+                delta = if !isnothing(terminator) terminator.stop else 0 end
+                offset -= delta
+                i += delta
+            else
+                @warn "Unrecognised ANSI code in string."
+                i = nextind(s, i)
+            end
+            continue
+        elseif s[i] == '\n'
+            push!(lines, @inbounds @view s[nextind(s, lastwrap):prevind(s, i)])
+            lastwrap = i
+            offset = 0
+        elseif i - (lastwrap - offset) > width
+            if lastwrap == mostrecentbreakoppotunity
+                nextbreak = findfirst(' ', @inbounds @view s[nextind(s, lastwrap):end])
+                if isnothing(nextbreak)
+                    mostrecentbreakoppotunity = slen
+                else
+                    mostrecentbreakoppotunity = nextbreak
+                end
+                i = mostrecentbreakoppotunity
+            end
+            push!(lines, @inbounds @view s[nextind(s, lastwrap):prevind(s, mostrecentbreakoppotunity)])
+            lastwrap = mostrecentbreakoppotunity
+            offset = 0
+        end
+        i = nextind(s, i)
+    end
+    if lastwrap < slen
+        push!(lines, @inbounds @view s[nextind(s, lastwrap):end])
+    end
+    lines
+end
