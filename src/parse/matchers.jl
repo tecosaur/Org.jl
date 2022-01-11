91ea73f651322fb38cbf642ecd8e0c572175d531
TEC <tec@tecosaur.com>
2022-01-12 02:29:39 +0800

    Matchers: Consume blank lines better

--- src/parse/matchers.jl
+++ src/parse/matchers.jl
@@ -7,7 +7,7 @@ orgmatcher(::Type{<:OrgComponent}) = nothing
 # ---------------------
 
 @inline orgmatcher(::Type{Heading}) = r"^(\*+)(?: +([A-Z]{2,}))?(?: +\[#([A-Z0-9])\])? +([^\n]*?)(?: +(:[A-Za-z0-9_\@#%][A-Za-z0-9_\@#%:]*:))?(?:[\n \t]*\n|$)((?:(?!\*+ )[^\n]+\n*)+)?"
-@inline orgmatcher(::Type{Section}) = r"^\n*((?:\n*?(?!\*+ )[^\n]+)+)(?:\n+|$)"
+@inline orgmatcher(::Type{Section}) = r"^\n*((?:\n*?(?!\*+ )[^\n]+)+)(?:\n(?:[ \t\r]*\n)*|$)"
 
 # ---------------------
 # Greater Elements
@@ -27,12 +27,12 @@ const OrgElementMatchers =
 const OrgElementFallbacks = [Paragraph, List]
 
 # Greater Block
-@inline orgmatcher(::Type{Drawer}) = r"^[ \t]*:([\w\-_]+):\n([\s\S]*?)\n?:END:(?:\n+|$)"i
+@inline orgmatcher(::Type{Drawer}) = r"^[ \t]*:([\w\-_]+):\n([\s\S]*?)\n?:END:(?:\n(?:[ \t\r]*\n)*|$)"i
 # Dynamic Block
 # FootnoteDef has a dedicated consumer
 # InlineTask
 @inline orgmatcher(::Type{PropertyDrawer}) = r"^[ \t]*:PROPERTIES:\n((?:[ \t]*:[^\+\n]+\+?:(?:[ \t]+[^\n]*|[ \t]*)?\n??)*)\n?[ \t]*:END:(?:[\n \t]*\n|$)"i
-@inline orgmatcher(::Type{Table}) = r"^([ \t]*\|[^\n]+(?:\n[ \t]*\|[^\n]+)*)((?:\n[ \t]*#\+TBLFM: [^\n]*)+)?(?:\n+|$)"
+@inline orgmatcher(::Type{Table}) = r"^([ \t]*\|[^\n]+(?:\n[ \t]*\|[^\n]+)*)((?:\n[ \t]*#\+TBLFM: [^\n]*)+)?(?:\n(?:[ \t\r]*\n)*|$)"
 # List and Item have dedicated consumers
 @inline orgmatcher(::Type{List}) = r"^([ \t]*)(\+|\-| \*|(?:[A-Za-z]|[0-9]+)[\.\)]) "
 @inline orgmatcher(::Type{Item}) = r"^([ \t]*)(\+|\-| \*|(?:[A-Za-z]|[0-9]+)[\.\)]) "
@@ -41,18 +41,18 @@ const OrgElementFallbacks = [Paragraph, List]
 # Elements
 # ---------------------
 
-@inline orgmatcher(::Type{BabelCall}) = r"^[ \t]*#\+call:[ \t]*([^\n]*)(?:\n+|$)"i
-@inline orgmatcher(::Type{Block}) = r"^[ \t]*#\+begin_(\S+)(?: ([^\n]+?))?[ \t]*?(?:\n((?!\*)[^\n]*(?:\n(?!\*)[^\n]*)*))?\n[ \t]*#\+end_\1(?:\n+|$)"i
-@inline orgmatcher(::Type{Clock}) = r"^[ \t]*clock: \[(\d{4}-\d\d-\d\d)(?: [A-Za-z]+)?(?: (\d?\d:\d\d)(?:-(\d?\d:\d\d))?)?(?: ((?:\+|\+\+|\.\+|-|--))([\d.]+)([hdwmy]))? *\](?(3)|(?:|-\[(\d{4}-\d\d-\d\d)(?: [A-Za-z]+)?(?: (\d?\d:\d\d))?(?: ((?:\+|\+\+|\.\+|-|--))([\d.]+)([hdwmy]))? *\]))(?:\n+|$)"i
+@inline orgmatcher(::Type{BabelCall}) = r"^[ \t]*#\+call:[ \t]*([^\n]*)(?:\n(?:[ \t\r]*\n)*|$)"i
+@inline orgmatcher(::Type{Block}) = r"^[ \t]*#\+begin_(\S+)(?: ([^\n]+?))?[ \t]*?(?:\n((?!\*)[^\n]*(?:\n(?!\*)[^\n]*)*))?\n[ \t]*#\+end_\1(?:\n(?:[ \t\r]*\n)*|$)"i
+@inline orgmatcher(::Type{Clock}) = r"^[ \t]*clock: \[(\d{4}-\d\d-\d\d)(?: [A-Za-z]+)?(?: (\d?\d:\d\d)(?:-(\d?\d:\d\d))?)?(?: ((?:\+|\+\+|\.\+|-|--))([\d.]+)([hdwmy]))? *\](?(3)|(?:|-\[(\d{4}-\d\d-\d\d)(?: [A-Za-z]+)?(?: (\d?\d:\d\d))?(?: ((?:\+|\+\+|\.\+|-|--))([\d.]+)([hdwmy]))? *\]))(?:\n(?:[ \t\r]*\n)*|$)"i
 # Planning has a custom consumer
-@inline orgmatcher(::Type{Comment}) = r"^([ \t]*#(?:| [^\n]*)(?:\n[ \t]*#(?:\n| [^\n]*))*)(?:\n+|$)"
-@inline orgmatcher(::Type{FixedWidth}) = r"^([ \t]*:(?:| [^\n]*)(?:\n[ \t]*:(?:\n| [^\n]*))*)(?:\n+|$)"
-@inline orgmatcher(::Type{HorizontalRule}) = r"^[ \t]*-{5,}[ \t]*(?:\n+|$)"
-@inline orgmatcher(::Type{Keyword}) = r"^[ \t]*#\+(\S+): *(.*)(?:\n+|$)"
-@inline orgmatcher(::Type{LaTeXEnvironment}) = r"^[ \t]*\\begin{([A-Za-z*]*)}\n(.*)\n[ \t]*\\end{\1}(?:\n+|$)"
-@inline orgmatcher(::Type{NodeProperty}) = r"^[ \t]*:([^\+\n]+)(\+)?:([ \t]+[^\n]*|[ \t]*)(?:\n+|$)"
-@inline orgmatcher(::Type{Paragraph}) = r"^[ \t]*+((?!\*+ |#\+\S|\[fn:([A-Za-z0-9-_]+)\] |[ \t]*(?:[*\-\+]|[A-Za-z]\.|\d+\.)[ \t]|:([\w\-_]+):(?:\n+|$)|\||#\n|# |:\n|: |[ \t]*\-{5,}[ \t]*(?:\n+|$)|\\begin\{)[^\n]+(?:\n[ \t]*+(?!\*+ |#\+\S|\[fn:([A-Za-z0-9-_]*)\] |[ \t]*(?:[*\-\+]|[A-Za-z]\.|\d+\.)[ \t]|:([\w\-_]+):(?:\n+|$)|\||#\n|# |:\n|: |[ \t]*\-{5,}[ \t]*(?:\n+|$)|\\begin\{)[^\n]+)*)(?:\n+|$)"
-@inline orgmatcher(::Type{TableRow}) = r"^[ \t]*(\|[^\n]*)(?:\n+|$)"
+@inline orgmatcher(::Type{Comment}) = r"^([ \t]*#(?:| [^\n]*)(?:\n[ \t]*#(?:\n| [^\n]*))*)(?:\n(?:[ \t\r]*\n)*|$)"
+@inline orgmatcher(::Type{FixedWidth}) = r"^([ \t]*:(?:| [^\n]*)(?:\n[ \t]*:(?:\n| [^\n]*))*)(?:\n(?:[ \t\r]*\n)*|$)"
+@inline orgmatcher(::Type{HorizontalRule}) = r"^[ \t]*-{5,}[ \t]*(?:\n(?:[ \t\r]*\n)*|$)"
+@inline orgmatcher(::Type{Keyword}) = r"^[ \t]*#\+(\S+): *(.*)(?:\n(?:[ \t\r]*\n)*|$)"
+@inline orgmatcher(::Type{LaTeXEnvironment}) = r"^[ \t]*\\begin{([A-Za-z*]*)}\n(.*)\n[ \t]*\\end{\1}(?:\n(?:[ \t\r]*\n)*|$)"
+@inline orgmatcher(::Type{NodeProperty}) = r"^[ \t]*:([^\+\n]+)(\+)?:([ \t]+[^\n]*|[ \t]*)(?:\n(?:[ \t\r]*\n)*|$)"
+@inline orgmatcher(::Type{Paragraph}) = r"^[ \t]*+((?!\*+ |#\+\S|\[fn:([A-Za-z0-9-_]+)\] |[ \t]*(?:[*\-\+]|[A-Za-z]\.|\d+\.)[ \t]|:([\w\-_]+):(?:\n(?:[ \t\r]*\n)*|$)|\||#\n|# |:\n|: |[ \t]*\-{5,}[ \t]*(?:\n(?:[ \t\r]*\n)*|$)|\\begin\{)[^\n]+(?:\n[ \t]*+(?!\*+ |#\+\S|\[fn:([A-Za-z0-9-_]*)\] |[ \t]*(?:[*\-\+]|[A-Za-z]\.|\d+\.)[ \t]|:([\w\-_]+):(?:\n(?:[ \t\r]*\n)*|$)|\||#\n|# |:\n|: |[ \t]*\-{5,}[ \t]*(?:\n(?:[ \t\r]*\n)*|$)|\\begin\{)[^\n]+)*)(?:\n(?:[ \t\r]*\n)*|$)"
+@inline orgmatcher(::Type{TableRow}) = r"^[ \t]*(\|[^\n]*)(?:\n(?:[ \t\r]*\n)*|$)"
 @inline orgmatcher(::Type{TableHrule}) = r"^|[\-\+]+|"
 
 # ---------------------
