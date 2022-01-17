4ce05b40e74beaad18c781b04fbf2668aca8f219
TEC <tec@tecosaur.com>
2022-01-17 15:15:51 +0800

    Make affiliated keyword wrapped element a param

--- src/types/affiliatedkeywords.jl
+++ src/types/affiliatedkeywords.jl
@@ -1,4 +1,4 @@
-struct AffiliatedKeywordsWrapper <: Element
-    element::Element
+struct AffiliatedKeywordsWrapper{E <: Element} <: Element
+    element::E
     keywords::Vector{AffiliatedKeyword}
 end
