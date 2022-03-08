tecosaur/Org.jl

commit 16a9be160a8bbbd957b971b8371485616918958c

Improve list parsing and rendering

src/parse/consumers.jl
@@ -182,18 +182,19 @@ function consume(::Type{Item}, text::AbstractString)
         if bullet == "*"
             indent *= " "
         end
+        contentindent = indent * "  "
         itemextras = match(r"^(?:[ \t]+\[\@([A-Za-z]|[0-9]+)\])?(?:[ \t]+\[([ \-X])\])?(?:[ \t]+([^\n]+?)[ \t]::)?[ \t]+",
                            @inbounds @view text[ncodeunits(itemstart.match):end])
         counterset, checkbox, tag = itemextras.captures
         # collect contents
         rest = @inbounds @view text[ncodeunits(itemstart.match) + ncodeunits(itemextras.match):end]
         contentlen, contentobjs = 0, OrgComponent[]
-        len, obj = itemconsume(rest, indent)
+        len, obj = itemconsume(rest, contentindent)
         contentlen += len
         push!(contentobjs, obj)
         rest = @inbounds @view text[contentlen + ncodeunits(itemstart.match) + ncodeunits(itemextras.match):end]
-        while startswith(rest, indent * "  ") && !isnothing(obj)
-            len, obj = itemconsume(rest, indent)
+        while startswith(rest, contentindent) && !isnothing(obj)
+            len, obj = itemconsume(rest, contentindent)
             contentlen += len
             push!(contentobjs, obj)
             rest = @inbounds @view text[contentlen + ncodeunits(itemstart.match) + ncodeunits(itemextras.match):end]
@@ -222,17 +223,21 @@ function consume(::Type{List}, text::AbstractString)
     itemstart = match(r"^([ \t]*)(\+|\-| \*|(?:[A-Za-z]|[0-9]+)[\.\)]) ", text)
     if !isnothing(itemstart)
         point = 1
+        listindent = itemstart.captures[1]
         items = Item[]
         nextitem = consume(Item, text)
-        typ = itemtype(nextitem[2])
+        listtype = itemtype(nextitem[2])
         while !isnothing(nextitem) && point < ncodeunits(text)
             len, item = nextitem
-            (itemtype(item) != typ) && break
+            (itemtype(item) != listtype) && break
             point += len
             push!(items, item)
-            nextitem = consume(Item, @inbounds @view text[point:end])
+            rest = @inbounds @view text[point:end]
+            nextitem = if startswith(rest, listindent)
+                consume(Item, rest)
+            end
         end
-        point-1, typ(items)
+        point-1, listtype(items)
     end
 end
 

src/parse/operations.jl
@@ -187,12 +187,16 @@ iterate(l::List, index::Integer) =
         (l.items[index], index + 1)
     end
 
-length(i::Item) = length(i.contents)
-iterate(i::Item) = if length(i) > 0 (i.contents[1], 2) end
-iterate(i::Item, index::Integer) =
-    if index <= length(i.contents)
-        (i.contents[index], index + 1)
+length(i::Item) = length(i.contents) + if isnothing(i.tag) 0 else length(i.tag) end
+iterate(i::Item) = if length(i) > 0 iterate(i, 1) end
+function iterate(i::Item, index::Integer)
+    taglen = if isnothing(i.tag) 0 else length(i.tag) end
+    if index <= taglen
+        (i.tag[index], index + 1)
+    elseif index - taglen <= length(i.contents)
+        (i.contents[index - taglen], index + 1)
     end
+end
 
 length(p::PropertyDrawer) = length(p.contents)
 iterate(p::PropertyDrawer) = if length(p) > 0 (p.contents[1], 2) end

src/render/org.jl
@@ -150,8 +150,8 @@ end
 # InlineTask
 
 function org(io::IO, item::Item, indent::Integer=0, offset::Integer=0)
-    print(io, ' '^indent, item.bullet)
-    offset += length(item.bullet)
+    print(io, ' '^indent, item.bullet, ' ')
+    offset += length(item.bullet) + 1
     if !isnothing(item.counterset)
         print(io, " [@", item.counterset, "]")
         offset += length(item.counterset) + 4
@@ -161,8 +161,20 @@ function org(io::IO, item::Item, indent::Integer=0, offset::Integer=0)
         offset += 4
     end
     if !isnothing(item.tag)
-        print(io, item.tag, " ::")
-        offset += length(item.tag) + 3
+        tagbuf = IOContext(IOBuffer(), :color => get(io, :color, false),
+                           :displaysize => (displaysize(io)[1],
+                                            displaysize(io)[2] - indent - 2))
+        for obj in item.tag
+            org(tagbuf, obj)
+        end
+        taglines = wraplines(String(take!(tagbuf.io)),
+                             displaysize(io)[2] - indent - 2, offset)
+        for line in taglines
+            print(io, line)
+            line === last(taglines) || print(io, '\n', ' '^(indent+2))
+        end
+        print(io, " ::")
+        offset += length(taglines[end]) + 3
     end
     if length(item.contents) > 0
         print(io, ' ')

src/render/term.jl
@@ -226,13 +226,13 @@ end
 
 # InlineTask
 
-function term(io::IO, o::Org, item::Item, unordered::Bool=true, indent::Integer=0, depth::Integer=0)
+function term(io::IO, o::Org, item::Item, ordered::Bool=false, indent::Integer=0, depth::Integer=0)
     print(io, ' '^indent)
     offset = indent
-    if unordered
-        printstyled(io, if depth % 2 == 0 '•' else '➤' end, color=:blue)
-    else
+    if ordered
         printstyled(io, item.bullet, color=:blue)
+    else
+        printstyled(io, if depth % 2 == 0 '•' else '➤' end, color=:blue)
     end
     offset += length(item.bullet)
     if !isnothing(item.checkbox)
@@ -242,11 +242,21 @@ function term(io::IO, o::Org, item::Item, unordered::Bool=true, indent::Integer=
     end
     if !isnothing(item.tag)
         print(io, ' ', termstyle("34"))
+        tagbuf = IOContext(IOBuffer(), :color => get(io, :color, false),
+                           :displaysize => (displaysize(io)[1],
+                                            displaysize(io)[2] - indent - 2))
         for obj in item.tag
-            term(io, o, obj, ["34"])
+            term(tagbuf, o, obj, ["34"])
+        end
+        taglines = wraplines(String(take!(tagbuf.io)),
+                             displaysize(io)[2] - indent - 2, offset)
+        for line in taglines
+            print(io, line)
+            line === last(taglines) || print(io, '\n', ' '^(indent+2))
         end
         print(io, " ::", termstyle())
-        offset += length(string(item.tag)) + 3 # TODO rough guess, make accurate
+        offset += textwidth(taglines[end]) -
+            ansi_escape_textwidth_offset(taglines[end]) + 3
     end
     if length(item.contents) > 0
         print(io, ' ')
@@ -274,14 +284,14 @@ function term(io::IO, o::Org, item::Item, unordered::Bool=true, indent::Integer=
         lines = vcat(parlines, if otherlines == [""]; [] else otherlines end)
         for line in lines
             print(io, line)
-            line === last(lines) || print(io, '\n', ' '^(indent+2))
+            line === last(lines) || print(io, '\n', ' '^indent)
         end
     end
 end
 
-function term(io::IO, o::Org, list::List, indent::Integer=0, depth=0)
+function term(io::IO, o::Org, list::List, indent::Integer=0, depth::Integer=0)
     for item in list.items
-        term(io, o, item, list isa UnorderedList, indent, depth)
+        term(io, o, item, list isa OrderedList, indent, depth)
         item === last(list.items) || print(io, '\n')
     end
 end

commit 6351d08a6dcdf8bb3d5cc97276ea0bda07cbc124

Parsing indents in items: I was right initially
The extra indenting in 16a9be1 introduced subtle issues where a single
paragraph was parsed line-by-line, breaking the parsing of objects that
spread across multiple lines.

src/parse/consumers.jl
@@ -200,19 +200,18 @@ function consume(::Type{Item}, text::AbstractString)
         if bullet == "*"
             indent *= " "
         end
-        contentindent = indent * "  "
         itemextras = match(r"^(?:[ \t]+\[\@([A-Za-z]|[0-9]+)\])?(?:[ \t]+\[([ \-X])\])?(?:[ \t]+([^\n]+?)[ \t]::)?[ \t]+",
                            @inbounds @view text[ncodeunits(itemstart.match):end])
         counterset, checkbox, tag = itemextras.captures
         # collect contents
         rest = @inbounds @view text[ncodeunits(itemstart.match) + ncodeunits(itemextras.match):end]
         contentlen, contentobjs = 0, OrgComponent[]
-        len, obj = itemconsume(rest, contentindent)
+        len, obj = itemconsume(rest, indent)
         contentlen += len
         push!(contentobjs, obj)
         rest = @inbounds @view text[contentlen + ncodeunits(itemstart.match) + ncodeunits(itemextras.match):end]
-        while startswith(rest, contentindent) && !isnothing(obj)
-            len, obj = itemconsume(rest, contentindent)
+        while startswith(rest, indent * "  ") && !isnothing(obj)
+            len, obj = itemconsume(rest, indent)
             contentlen += len
             push!(contentobjs, obj)
             rest = @inbounds @view text[contentlen + ncodeunits(itemstart.match) + ncodeunits(itemextras.match):end]