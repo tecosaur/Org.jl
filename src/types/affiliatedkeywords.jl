52e384ad0541be7b3d546330d173dc352700d1e4
TEC <tec@tecosaur.com>
2022-03-29 14:55:37 +0800

    Allow for AffiliatedKeyword subtyping in wrapper

--- src/types/affiliatedkeywords.jl
+++ src/types/affiliatedkeywords.jl
@@ -1,6 +1,6 @@
 struct AffiliatedKeywordsWrapper{E <: Element} <: Element
     element::E
-    keywords::Vector{AffiliatedKeyword}
+    keywords::Vector{<:AffiliatedKeyword}
 end
 
 AffiliatedKeywordsWrapper(element::Element, keywords::Vector{<:Pair{String, <:Any}}) =
