347ced30ec20b06d9c72d0921bc5f073ab1746cf
TEC <tec@tecosaur.com>
2022-03-29 15:04:16 +0800

    Add section and heading constructors

--- src/types/sectioning.jl
+++ src/types/sectioning.jl
@@ -4,6 +4,8 @@ mutable struct Section <: Element
     properties::Union{PropertyDrawer, Nothing}
 end
 
+Section(contents::Vector=Element[]) = Section(Vector{Element}(contents), nothing, nothing)
+
 mutable struct Heading <: Element
     level::Integer
     keyword::Union{SubString{String}, Nothing}
@@ -12,3 +14,17 @@ mutable struct Heading <: Element
     tags::Vector{SubString{String}}
     section::Union{Section, Nothing}
 end
+
+Heading(level::Integer, title::Vector{Object}, section::Union{Section, Nothing}=nothing;
+        keyword::Union{String, Nothing}=nothing,
+        priority::Union{String, Nothing}=nothing,
+        tags::Vector{String}=String[]) =
+            Heading(level, if !isnothing(keyword) SubString(keyword) end,
+                    if !isnothing(priority) SubString(priority) end,
+                    title, SubString.(tags), section)
+
+Heading(level::Integer, title::String, section::Union{Section, Nothing}=nothing;
+        keyword::Union{String, Nothing}=nothing,
+        priority::Union{String, Nothing}=nothing,
+        tags::Vector{String}=String[]) =
+            Heading(level, Object[TextPlain(title)], section; keyword, priority, tags)
