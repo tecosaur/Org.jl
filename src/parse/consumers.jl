1213b6f1abd11c7aa49fdf8060b50cf342930034
TEC <git@tecosaur.net>
2024-09-29 02:42:17 +0800

    Recognise markup at start of link description
    This fix technically introduces a (very) small syntactic divergence, but
    it relates to a larger issue that will really need a rewrite to solve.

--- src/parse/consumers.jl
+++ src/parse/consumers.jl
@@ -541,7 +541,9 @@ function consume(::Type{TextMarkup}, text::SubString{String})
         if text isa SubString
             text.offset == 0 ||
             text.string[prevind(text.string, 1+text.offset)] in
-            ('-', '(', '{', ''', '"', ' ', '\t', '\n', '\u2000':'\u200c'...) # pre condition
+            ('-', '(', '{', ''', '"', ' ', '\t', '\n', '\u2000':'\u200c'...) || # pre condition
+            (text.string[prevind(text.string, 1+text.offset)] == '[' &&
+            text.string[prevind(text.string, text.offset)] == ']') # link description
         else
             true
         end
